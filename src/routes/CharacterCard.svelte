3c4003f0cb4f095d56012a8fea3379a93f616f7c
Sascha Dörflein <48054686+SaschaDoe@users.noreply.github.com>
2023-06-11 06:47:29 +0200

    Added reusable Modal component

--- src/routes/CharacterCard.svelte
+++ src/routes/CharacterCard.svelte
@@ -1,6 +1,8 @@
 <script>
     import {Character} from "$lib/entities/Character.ts";
     import { createEventDispatcher } from 'svelte';
+    import Modal from './Modal.svelte';
+    let showModal = false;
 
     export let character = new Character();
 
@@ -12,4 +14,21 @@
     }
 </script>
 <h2>Character with id {character.id}</h2>
-<button on:click={deleteThisCharacter}>Delete</button>
+<button class="delete-button" on:click={() => showModal = true}>Delete</button>
+{#if showModal}
+    <Modal on:close={() => showModal = false}>
+        <h2>Do you want Character {character.id} to delete?</h2>
+        <button on:click={() => { deleteThisCharacter(); showModal = false }}>Yes</button>
+        <button on:click={() => showModal = false}>No</button>
+    </Modal>
+{/if}
+<style>
+    .delete-button {
+        background-color: red;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        padding: 5px;
+        margin: 5px;
+    }
+</style>
---
+++ src/routes/Modal.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+
+    function closeModal() {
+        dispatch('close');
+    }
+</script>
+
+<div class="modal-background" on:click={closeModal}>
+    <div class="modal-content" on:click={(e) => e.stopPropagation()}>
+        <slot></slot>
+    </div>
+</div>
+
+<style>
+    .modal-background {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0,0,0,0.5);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .modal-content {
+        background-color: #fff;
+        padding: 20px;
+        border-radius: 8px;
+        max-width: 90%;
+        box-shadow: 0 2px 10px rgba(0,0,0,0.2);
+    }
+</style>
