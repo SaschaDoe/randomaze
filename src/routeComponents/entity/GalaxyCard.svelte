fa464a9adc63133007a559839e90b0bf310dafba
Sascha Dörflein <48054686+SaschaDoe@users.noreply.github.com>
2023-06-15 06:00:39 +0200

    fix responsive design

--- src/routeComponents/entity/GalaxyCard.svelte
+++ src/routeComponents/entity/GalaxyCard.svelte
@@ -1,54 +1,88 @@
 <script>
-
     import GalaxyDetails from "./GalaxyDetails.svelte";
 
     export let galaxy;
 
-    let currentTab = 'details';  // 'details', 'systems', 'anomalies'
-
+    let currentTab = 'details'; // 'details', 'systems', 'anomalies'
     let tabs = ['details', 'systems', 'anomalies'];
     let currentIndex = 0;
 
+    let isMobile = window.innerWidth <= 700;
+    let showSection = isMobile ? 'info' : 'both'; // Default to 'info'
+    let lastMobileSection = 'info'; // Remember last shown section on mobile
+
     function nextTab() {
-        if(currentIndex < tabs.length - 1) {
+        if (currentIndex < tabs.length - 1) {
             currentIndex += 1;
             currentTab = tabs[currentIndex];
         }
     }
 
     function prevTab() {
-        if(currentIndex > 0) {
+        if (currentIndex > 0) {
             currentIndex -= 1;
             currentTab = tabs[currentIndex];
         }
     }
+
+    function toggleSection(section) {
+        showSection = section;
+        if (isMobile) lastMobileSection = section; // Update last shown section on mobile
+    }
+
+    window.addEventListener('resize', () => {
+        isMobile = window.innerWidth <= 700;
+        if (!isMobile) {
+            showSection = 'both';
+        } else {
+            showSection = lastMobileSection; // Restore last shown section when resizing back to small
+        }
+    });
 </script>
 
-<link href='https://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet' type='text/css'>
+
 <div class="container">
-    <div class="image-section">
-        <div class="galaxy-name-id">{galaxy.id}: {galaxy.name}</div>
-        <div class="image-container">
-            <img class="galaxy-image" src={galaxy.imagePath} alt="Random Galaxy">
-            <div class="color-overlay" style="background: {galaxy.color};"></div>
+    {#if showSection === 'image' || showSection === 'both'}
+        <div class="image-section">
+            <div class="galaxy-name-id">{galaxy.id}: {galaxy.name}</div>
+            <div class="image-container">
+                <img class="galaxy-image" src={galaxy.imagePath} alt="Random Galaxy">
+                <div class="color-overlay" style="background: {galaxy.color};"></div>
+            </div>
         </div>
-    </div>
+    {/if}
 
-    <div class="entity-information">
-        <div class="navigation">
-            <button on:click={prevTab} disabled={currentIndex === 0} class="nav-button">◀</button>
-            <h2>{currentTab.charAt(0).toUpperCase() + currentTab.slice(1)}</h2>
-            <button on:click={nextTab} disabled={currentIndex === tabs.length - 1} class="nav-button">▶</button>
+    {#if showSection === 'info' || showSection === 'both'}
+        <div class="entity-information">
+            <div class="navigation">
+                <button on:click={prevTab} disabled={currentIndex === 0} class="nav-button">◀</button>
+                <h2>{currentTab.charAt(0).toUpperCase() + currentTab.slice(1)}</h2>
+                <button on:click={nextTab} disabled={currentIndex === tabs.length - 1} class="nav-button">▶</button>
+            </div>
+
+            {#if currentTab === 'details'}
+                <GalaxyDetails galaxy={galaxy}></GalaxyDetails>
+            {:else if currentTab === 'systems'}
+                <p>No System</p>
+            {:else if currentTab === 'anomalies'}
+                <p>No anomaly</p>
+            {/if}
         </div>
+    {/if}
 
-        {#if currentTab === 'details'}
-            <GalaxyDetails galaxy={galaxy}></GalaxyDetails>
-        {:else if currentTab === 'systems'}
-            <p>No System</p>
-        {:else if currentTab === 'anomalies'}
-           <p>No anomaly</p>
-        {/if}
-    </div>
+    {#if isMobile}
+        <div class="navigation-buttons">
+            {#if showSection === 'info'}
+                <button on:click={() => toggleSection('image')} class="nav-button">&lt;&lt;</button>
+                <div></div> <!-- Placeholder for alignment -->
+            {/if}
+            {#if showSection === 'image'}
+                <div></div> <!-- Placeholder for alignment -->
+                <button on:click={() => toggleSection('info')} class="nav-button">&gt;&gt;</button>
+
+            {/if}
+        </div>
+    {/if}
 </div>
 
 <style>
@@ -62,6 +96,7 @@
         justify-content: space-between;
         cursor: crosshair;
     }
+
     .image-section {
         display: flex;
         flex-direction: column;
@@ -117,5 +152,29 @@
     .nav-button:disabled {
         color: gray;
     }
+    @media (max-width: 700px) {
+        .container {
+            flex-direction: column;
+            align-items: center;
+        }
 
+        .navigation-buttons {
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
+            margin-top: 10px;
+        }
+    }
+
+    @media (min-width: 700px) {
+        .navigation-buttons {
+            display: none;
+        }
+    }
+    .navigation-buttons {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        margin-top: 10px;
+    }
 </style>
