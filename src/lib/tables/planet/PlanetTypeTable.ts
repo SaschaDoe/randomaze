355ea67c8921079aa20a7c380a03625abe860122
Sascha Dörflein <48054686+SaschaDoe@users.noreply.github.com>
2023-06-20 16:29:43 +0200

    fixed planet type jungle bug

--- src/lib/tables/planet/PlanetTypeTable.ts
+++ src/lib/tables/planet/PlanetTypeTable.ts
@@ -1,8 +1,9 @@
 import {Table} from "../Table";
 
 export const PlanetTypes = [
-    "desert", "ice", "djungle", "earthlike", "gasgiant", "puffy", "lava", "ocean", "rocky",
+    "desert", "ice", "jungle", "earthlike", "gasgiant", "puffy", "lava", "ocean", "rocky",
 ];
+
 export class PlanetTypeTable extends Table{
     constructor() {
         super("Planet Types");
