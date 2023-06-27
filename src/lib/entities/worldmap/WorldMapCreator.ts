7ba3512f9087b0474e9294ae01cf96d12bda0109
Sascha Dörflein <48054686+SaschaDoe@users.noreply.github.com>
2023-06-27 05:28:39 +0200

    cleaner square map

--- src/lib/entities/worldmap/WorldMapCreator.ts
+++ src/lib/entities/worldmap/WorldMapCreator.ts
@@ -3,52 +3,48 @@ import {WorldMap} from "./WorldMap";
 import {makeNoise2D} from "open-simplex-noise";
 import {Dice} from "../../tables/Dice";
 
-export class WorldMapCreator{
+export class WorldMapCreator {
+    private static DEFAULT_WIDTH = 50;
+    private static DEFAULT_HEIGHT = 50;
+    private static DEFAULT_NOISE_SCALE = 3.0;
+    private static DEFAULT_ELEVATION_SCALE = 7;
 
-    static create(planet: Planet, dice?: Dice){
-        if(!dice) {
-            dice = new Dice();
-        }
-        let worldMap = new WorldMap();
+    static create(planet: Planet, dice = new Dice(), width = WorldMapCreator.DEFAULT_WIDTH, height = WorldMapCreator.DEFAULT_HEIGHT) {
+        const worldMap = new WorldMap();
 
-        let width = 50;
-        let height = 50;
         worldMap.seed = dice.roll(65536);
         worldMap.map = this.createMap(planet, width, height, worldMap.seed);
 
         planet.worldMap = worldMap;
-        
+
         return worldMap;
     }
 
-
     private static createMap(planet: Planet, width: number, height: number, seed: number) {
-        let map : number[][] = [];
-        let noiseScale = 2.0;
-        let elevationScale = 5;
-
-        // Create Simplex noise generator
+        const map: number[][] = [];
         const noise2D = makeNoise2D(seed);
-
-        let edgeFade = 0.4; // Determines how quickly the land fades to water at the map edges. Lower values mean larger landmass
 
         for (let i = 0; i < width; i++) {
             map[i] = [];
             for (let j = 0; j < height; j++) {
-                let nx = i / width * 2 - 1, ny = j / height * 2 - 1;
-
-                let distanceToCenter = Math.sqrt(nx * nx + ny * ny);
-                let islandFactor = Math.pow(Math.max(0, 1 - edgeFade * distanceToCenter), 2); // Calculate the island factor
-
-                let elevation = noise2D(nx * noiseScale, ny * noiseScale);
-                elevation = Math.abs(elevation);
-                elevation = Math.floor(elevation * islandFactor * elevationScale);
-
-                map[i][j] = elevation;
+                map[i][j] = this.calculateElevation(i, j, width, height, noise2D);
             }
         }
 
         return map;
     }
 
+    private static calculateElevation(i: number, j: number, width: number, height: number, noise2D: (x: number, y: number) => number) {
+        let nx = i / width * 2 - 1;
+        let ny = j / height * 2 - 1;
+
+        let distanceToCenter = Math.sqrt(nx * nx + ny * ny);
+        let islandFactor = Math.pow(1 - Math.min(1, distanceToCenter), 0.8);
+
+        let elevation = noise2D(nx * this.DEFAULT_NOISE_SCALE, ny * this.DEFAULT_NOISE_SCALE);
+        elevation = (elevation + 1) / 2;
+        elevation = Math.floor(elevation * islandFactor * this.DEFAULT_ELEVATION_SCALE);
+
+        return elevation;
+    }
 }
